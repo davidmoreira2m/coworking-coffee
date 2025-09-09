cd012b8e98574fd3e6c71ae96819603e077f9e6b
dmaia <dmaia@tce.pb.gov.br>
2025-09-09 15:53:01 -0300

    segundo commit

---
+++ src/components/navbar/menuitem.js
@@ -0,0 +1,17 @@
+export const Menu = [
+  {
+    id: 1,
+    name: 'Home',
+    link: '/#',
+  },
+  {
+    id: 2,
+    name: 'Serviços',
+    link: '/#services',
+  },
+  {
+    id: 3,
+    name: 'Sobre Nós',
+    link: '/#about',
+  },
+];
