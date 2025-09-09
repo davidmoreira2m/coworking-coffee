ab2be60b2b47871ea6a42946173b56efd1726eb5
dmaia <dmaia@tce.pb.gov.br>
2025-09-09 17:04:34 -0300

    navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import Navbar from './components/navbar/Navbar';
+import Navbar from './components/navbar/Navbar.jsx';
 
 const App = () => {
   return (
     <div>
-      <Navbar></Navbar>
+      <Navbar />
     </div>
   );
 };
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,40 @@
+import Logo from '../../assets/website/logo.svg';
+import { Menu } from './menuitem';
+import { FaCoffee } from 'react-icons/fa';
+import './navbar.css';
+
+const Navbar = () => {
+  return (
+    <div className="navbar-container">
+      <div className="navbar-content">
+        {/* Logo Section */}
+        <div className="container-logo">
+          <a href="/" className="redirec-link">
+            <img src={Logo} alt="Logo" className="image-logo" />
+            <div className="content-text">
+              <span className="text-logo">Code Crux</span>
+              <span className="text-logo">Coworking Café</span>
+            </div>
+          </a>
+        </div>
+        {/* Links section */}
+        <div className="container-links">
+          <ul className="list-links">
+            {Menu.map((menu) => (
+              <li key={menu.id} className="link">
+                <a href={menu.link} className="link-name">
+                  {menu.name}
+                </a>
+              </li>
+            ))}
+          </ul>
+          <button className="cart-button">
+            Carrinho <FaCoffee />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
